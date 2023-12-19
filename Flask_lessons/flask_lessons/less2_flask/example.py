56fd4f33deae259c727926d00dbae9df297c69a4
Bkorob <bakatest311@yandex.ru>
2023-12-19 19:19:38 +0300

    questions form

--- Flask_lessons/flask_lessons/less2_flask/example.py
+++ Flask_lessons/flask_lessons/less2_flask/example.py
@@ -1,7 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for, session, flash, jsonify
+
+
+users = [
+    'mike',
+    'mishel',
+    'adel',
+    'keks',
+    'kamila'
+]
+
 
 # создаем экз класса Flask и передаем в него имя модуля
 app = Flask(__name__) 
+
 
 @app.route('/') # декоратор для обработки запросов к серверу
 def hello_world():
@@ -15,13 +26,24 @@ def hello_world():
 #     else:
 #         return "Hello from GET /users"
 
-#процесс указания метода с помощью отдельных декораторов
-@app.get('/users')
-def users_g():
-    return "Hello from GET /users"
-#для каждого метода декоратор отдельный
-@app.post('/users')
-def users_p():
-    return "Users", 418
+# #процесс указания метода с помощью отдельных декораторов
+# @app.get('/users')
+# def users_g():
+#     return "Hello from GET /users"
 
-    
+
+#для каждого метода декоратор отдельный
+# @app.post('/users')
+# def users_p():
+#     return "Users", 418
+
+
+@app.get('/users')
+def get_users():
+    # term = request.args.get('term')
+    # res = [user for user in users if term in user]
+    return render_template(
+        '/users/index.html',
+        # search=res,
+        # term=term,
+        )
